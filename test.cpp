d783cd61f3d5a32b01dbce78bb6611742d860e30
poporthailand <6206021612130@fitm.kmutnb.ac.th>
2019-06-26 00:04:27 +0700

    initial commit

---
+++ test.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	string name = "Ismat roongruang";
+	cout << "My name is : " << name << endl;
+	short scores1 = 100;
+	cout << "My scores 1 : " << scores1 << " points\n";
+	short scores2 = 99;
+	cout << "My socres 2 : " << scores2 << " points\n";
+	cout  << "Total : " << scores1 + scores2 << " points\n";
+	char grade = 'A';
+	cout << "Grade : " << grade << endl;
+	float gpa = 4.00;
+	cout << "Gpa : " << gpa << endl;
+	return 0;
+}
